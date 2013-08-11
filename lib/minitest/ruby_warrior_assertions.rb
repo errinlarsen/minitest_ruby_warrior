5ff6fc9b1ce4356556c930192c8e28a1f6d2a759
Errin Larsen <elarsen@insightcards.com>
2013-08-11 10:27:15 -0500

    [0.0.2] Handle when no scores or bonueses are generated

--- lib/minitest/ruby_warrior_assertions.rb
+++ lib/minitest/ruby_warrior_assertions.rb
@@ -9,40 +9,44 @@ module Minitest
     end
 
     def assert_ruby_warrior_level_score_meets_or_beats(score)
+      refute_nil (level_score = find_level_score), "Expected to find a Level score"
       assert_operator score, :<=, level_score
     end
 
     def assert_ruby_warrior_time_bonus_meets_or_beats(bonus)
+      refute_nil (time_bonus = find_time_bonus), "Expected to find a Time bonus"
       assert_operator bonus, :<=, time_bonus
     end
 
     def assert_ruby_warrior_clear_bonus_meets_or_beats(bonus)
+      refute_nil (clear_bonus = find_clear_bonus), "Expected to find a Clear bonus"
       assert_operator bonus, :<=, clear_bonus
     end
 
     def assert_ruby_warrior_total_score_meets_or_beats(score)
+      refute_nil (total_score = find_total_score), "Expected to find a Total score"
       assert_operator score, :<=, total_score
     end
 
     private
-    def level_score
+    def find_level_score
       match_data = /^Level Score: (?<score>\d+)$/.match(rw_output)
-      return match_data.nil? ? 0 : match_data[:score].to_i
+      return match_data ? match_data[:score].to_i : nil
     end
 
-    def time_bonus
+    def find_time_bonus
       match_data = /^Time Bonus: (?<bonus>\d+)$/.match(rw_output)
-      return match_data.nil? ? 0 : match_data[:bonus].to_i
+      return match_data ? match_data[:bonus].to_i : nil
     end
 
-    def clear_bonus
+    def find_clear_bonus
       match_data = /^Clear Bonus: (?<bonus>\d+)$/.match(rw_output)
-      return match_data.nil? ? 0 : match_data[:bonus].to_i
+      return match_data ? match_data[:bonus].to_i : nil
     end
 
-    def total_score
+    def find_total_score
       match_data = /^Total Score.*= (?<score>\d+)$/.match(rw_output)
-      return match_data.nil? ? 0 : match_data[:score].to_i
+      return match_data ? match_data[:score].to_i : nil
     end
   end
 end
